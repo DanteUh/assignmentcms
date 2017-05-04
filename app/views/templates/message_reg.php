aebd59b5c17bddcef965461ab1f3ea29d2be6132
AnnaLovisa <gribbe.anna@gmail.com>
2017-05-04 12:54:38 +0200

    changing

---
+++ app/views/templates/message_reg.php
@@ -0,0 +1,6 @@
+
+<?php include VIEW_ROOT . '/templates/header.php'; ?>
+
+<h1>Du är reggad!</h1>
+
+<?php include VIEW_ROOT . '/templates/footer.php'; ?>
--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<!--Index sköter första requesten -->
 <?php
 
 include 'app/database.php';
@@ -5,3 +6,6 @@ include 'app/database.php';
 $statement = $pdo->prepare("SELECT * FROM posts");
 $statement->execute();
 $posts = $statement->fetchAll();
+
+//När vi har alla posts, skickar vi vidare dessa till home
+include VIEW_ROOT .  '/home.php';
